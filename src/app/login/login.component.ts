5261aba0ca1cb27441565a766231402d9ced2799
rasmi-aw <abdelwadoudrasmi@gmail.com>
2024-03-04 11:50:55 +0100

    init

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  login: string
+  password: string
+
+  constructor() {
+    this.login = ""
+    this.password = ""
+  }
 }
